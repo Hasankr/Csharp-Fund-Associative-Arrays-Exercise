6eaa2fa7e28d6cf746a63ddf787f0e63e27e86b0
hasankr <hasankr@abv.bg>
2022-07-20 23:02:40 +0300

    added 7

---
+++ 07. Company Users/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07._Company_Users
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var companies = new Dictionary<string, List<string>>();
+            string cmd = Console.ReadLine();
+            while (cmd != "End")
+            {
+                string[] tokens = cmd.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+                var companyName = tokens[0];
+                var employeeID = tokens[1];
+                if (!companies.ContainsKey(companyName))
+                {
+                    companies.Add(companyName, new List<string>());
+                }
+                
+                    if (companies[companyName].Contains(employeeID))
+                {
+                    cmd = Console.ReadLine();
+                    continue;
+                }
+                   
+                companies[companyName].Add(employeeID);
+                cmd = Console.ReadLine();
+            }
+            foreach (var company in companies)
+            {
+                Console.WriteLine($"{company.Key}");
+                foreach (var employeeID in company.Value)
+                {
+                    Console.WriteLine($"-- {employeeID}");
+                }
+            }
+        }
+    }
+}
